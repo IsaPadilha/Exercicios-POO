cd4edd6d4309d3a0f2d6d847aa6ad9ed120ac5e7
Isadora Padilha <isap4619@gmail.com>
2026-05-10 18:36:25 -0300

    Exercicio01 feito

---
+++ Exercicio01.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Exercicio01.ConsoleApp;
+
+CaixaRetangular caixa = new CaixaRetangular();
+caixa.comprimento = 50;
+caixa.largura = 30;
+caixa.altura = 20;
+
+decimal volumeCaixa = caixa.ObterVolume();
+
+Console.WriteLine($"O volume da caixa é de: {volumeCaixa.ToString("F2")} cm³");
